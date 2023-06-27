4313d7b2ed61f0252d02463035410fca46d6cf52
“lhz” <972258425@qq.com>
2023-06-27 11:03:54 +0800

    feat:config

--- next.config.js
+++ next.config.js
@@ -16,10 +16,31 @@ const rules = [
     ],
   }
 ]
+// 用于为静态资源（如图像、样式表、JavaScript 文件等）设置 URL 前缀
+// 这在将应用部署到自定义域名或 CDN 上时特别有用，因为它允许您将静态资源存储在不同的位置
+let assetPrefix = ``
+
+// 用于为应用设置基础路径
+// 这在将应用部署到子目录下时特别有用，因为它允许您指定应用所在的目录
+let basePath = `/`
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 
 const nextConfig = {
   reactStrictMode: true,
   distDir: 'out',
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   webpack: (
     config,
     { buildId, dev, isServer, defaultLoaders, webpack },
